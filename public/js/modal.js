3e7a68545dc87f8bcb4b1f4e3f8cc48885dd3331
patilved01@gmail.com <patilved01@gmail.com>
2026-01-26 00:13:28 +0530

    Added Donate Modal popup UI

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,9 @@
+// Open modal
 function openDonateModal() {
   document.getElementById("donateModal").style.display = "flex";
 }
 
+// Close modal
 function closeDonateModal() {
   document.getElementById("donateModal").style.display = "none";
 }
